e101d9bcc3fffc6565c629746f40767c95cdd8b3
Loïc Harry LEENAERTS <loic_leenaerts@reseau.eseo.fr>
2025-06-04 18:47:48 +0200

    refonte du système de contrôle des moteurs

--- app/src/motor_control.h
+++ app/src/motor_control.h
@@ -1,9 +1,15 @@
-// motor_control.h
+// --------------------------------------------------
+// Exemple de fichier d'en-tête associé : motor_control_simple.h
+// --------------------------------------------------
+
 #ifndef MOTOR_CONTROL_H
 #define MOTOR_CONTROL_H
 
 #include <stdbool.h>
+#include <stdint.h>
 
 bool init_motors(void);
+void configure_timer_pwm(void);
+void changer_impulsion(uint16_t pulse);
 
 #endif // MOTOR_CONTROL_H
